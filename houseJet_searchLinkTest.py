b6f00a782df156fe4219cc79680aee71e68402b5
anu-ban <42361403+anu-ban@users.noreply.github.com>
2023-09-16 01:17:48 -0500

    HouseJet commit click_search_button

--- houseJet_searchLinkTest.py
+++ houseJet_searchLinkTest.py
@@ -3,19 +3,22 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+def click_search_button(driver):
+  wait = WebDriverWait(driver, 10)
+  nav_drawer = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "v-navigation-drawer--right")))
+  print(nav_drawer.location)
+  search_button = nav_drawer.find_element(By.PARTIAL_LINK_TEXT, "Search")
+  search_button.click()
+
 # Initialize the web driver (you should replace 'path_to_chromedriver' with your actual driver path)
-driver = webdriver.Chrome()
+driver = webdriver.Firefox()
 
 # Navigate to the housejet website
 driver.get("https://housejet.com/")
 
-# Use an explicit wait to wait for the navigation drawer to become visible
-wait = WebDriverWait(driver, 10)  # Adjust the timeout as needed
-nav_drawer = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "v-navigation-drawer--right")))
-
-# Find and click the "Search" button within the navigation drawer
-search_button = nav_drawer.find_element(By.PARTIAL_LINK_TEXT, "Search")  # Assuming the text "Search" is part of the link text
-search_button.click()
-
-# Close the web driver (you can remove this line if you want to keep the browser window open)
-driver.quit()
+try:
+  # Click the "Search" button
+  click_search_button(driver)
+except Exception as e:
+  print(e)
+  driver.quit()
