fb6074ddbc01f37fc5fe44e00df748c6e688fa9e
anu-ban <42361403+anu-ban@users.noreply.github.com>
2023-09-16 20:10:45 -0500

    Search input='65807' then shows the search result

--- test_houseJet.py
+++ test_houseJet.py
@@ -1,33 +1,28 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
 import time
-import requests
 
-from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
 
-response = requests.get("https://housejet.com/")
-soup = BeautifulSoup(response.text, features="html.parser")
-links = soup.find_all('a')
-for link in links:
-    print(link.text, link.get('href'))
-search_id = soup.find(link,'Search')
-print(search_id)
-#
-# with open('houseJet.txt', 'w',encoding = 'utf8') as f:
-#     f.write(soup.prettify())
-
+# Initialize the web driver
 driver = webdriver.Chrome()
-driver.get("https://housejet.com/")
-#
-# soup2 = BeautifulSoup(driver.page_source,features="html.parser")
-# with open('houseJetWebDriver.txt','w',encoding='utf8') as f:
-#     f.write(soup2.prettify())
-# Get package title
-title = driver.title
-#
-# Test if title is correct
-assert "HouseJet - HouseJet" in title
-print("TEST 0: 'title' test Passed ")
 
-# search_link.send_keys(Keys.RETURN)
+# Navigate to the HouseJet website
+
+
+# Find the element with the XPath selector ".//a[@href='/search']"
+# search_button = driver.find_element(By.XPATH(".//a[@href='/search']"))
+
+driver.get("https://housejet.com/Search")
+
+search_stuff = driver.find_element(By.ID,"googleAutocomplete")
+search_stuff.send_keys('65807')
+search_stuff.send_keys(Keys.ENTER)
+
+time.sleep(15)
+
+# Click the element
+
+
+# Close the web driver
+driver.quit()
