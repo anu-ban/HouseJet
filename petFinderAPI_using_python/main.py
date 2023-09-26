ec70ac7cbbd211570c89584709d96e142751cac5
anu-ban <42361403+anu-ban@users.noreply.github.com>
2023-09-26 13:17:10 -0500

    Now the petfinder API looks for available cats for adoption and writes the cat.txt file, I have also put a print function just to show how print statement can be used too

--- petFinderAPI_using_python/main.py
+++ petFinderAPI_using_python/main.py
@@ -13,15 +13,34 @@ with open("perfinderSecretKey.txt") as file:
 pf = Petfinder(key=apiKey, secret=secretKey)
 
 # Get a list of all the cats that are available for adoption
-cats = pf.animals(animal_type='cat',pages=3)
 
-# Get the names of all the cats
-cat_names = []
-for cat in cats['animals']:
-  cat_names.append(cat['name'])
-  str_output =  pd.DataFrame(cat_names)
+
+# Get the list of cats available for adoption, for 3 pages
+cats=[]
+for page in range(1,4):
+  cats.extend(pf.animals(animal_type='cat',pages=page,results_per_page=50,location='MO',status='adoptable')['animals'])
+
+with open('cat.txt','w',encoding='utf8') as f:
+  for cat in cats:
+    f.write(f"Id: {cat['id']}\n")
+    f.write(f"Kitty: {cat['name']}\n")
+    f.write(f"Attitude: {cat['description']}\n")
+    f.write(f"Status: {cat['status']}\n")
+    f.write(f"City: {cat['contact']['address']['city']}\n")
+    f.write(f"State: {cat['contact']['address']['state']}\n\n")
+
+for cat in cats:
+    print(f"Id: {cat['id']}")
+    print(f"Kitty: {cat['name']}")
+    print(f"Attitude: {cat['description']}")
+    print(f"Breed: {cat['status']}")
+    print(f"City: {cat['contact']['address']['city']}")
+    print(f"State: {cat['contact']['address']['state']}")
+    print()  # Empty print statement for a new line
+
+
 # Print the list of cat names
   # This is just to make it easier on yourself when you're debugging!
 
 
-print(str_output)
+
